44ea2662b0169bd5a2ba33fd8675fe5188f22e72
MazumalaAja <programlia1108@gmail.com>
2026-01-08 06:16:44 +0700

    continue api and ajax materials

--- modules/Javascript-Ajax/app.js
+++ modules/Javascript-Ajax/app.js
@@ -18,3 +18,21 @@ console.log(JSON.parse(data));
 
 // Mengubah Object javascript jadi JSON...
 console.log(JSON.stringify(data2));
+
+
+async function getApi(params) {
+     const data = await fetch(params);
+     const response = await data.json();
+     response.data.forEach(element => {
+          console.log(element.nomor);
+          console.log(element.nama);
+          console.log(element.namaLatin);
+     });
+}
+
+// getApi("https://equran.id/api/v2/surat");
+
+
+// Menggunakan Axios...
+axios.get("https://equran.id/api/v2/surat").then(res => console.log(res)).catch(err => console.log(err.message)
+);
